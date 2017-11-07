940b84da0e991f24bc341693d94e340f255cfc91
Matthew Soulanille <matthew@soulanille.net>
2017-11-06 20:34:16 -0500

    wrote reverse

--- uppercase_reverse.py
+++ uppercase_reverse.py
@@ -1,5 +1,8 @@
 def uppercase(a_string):
     return a_string.upper()
 
+def reverse(a_string):
+    return a_string[::-1]
+
 def uppercase_reverse(a_string):
     return uppercase(reverse(a_string))
